767c09a7d11d48488742dda17854b06a05324a1f
Carlos Eduardo Ribeiro <mail@carlosribeiro.me>
2015-12-17 18:53:46 -0300

    Fizzbuzz exercise

--- first_steps/ex_functions_2_3.exs
+++ first_steps/ex_functions_2_3.exs
@@ -5,4 +5,15 @@ fizz_buzz = fn
   _, _, x -> x
 end
 
-IO.puts fizz_buzz.(1,1,2)
+fizz_word = fn(n) ->
+  fizz_buzz.(rem(n,3), rem(n,5), n)
+end
+
+#IO.puts fizz_buzz.(1,1,2)
+IO.puts fizz_word.(10)
+IO.puts fizz_word.(11)
+IO.puts fizz_word.(12)
+IO.puts fizz_word.(13)
+IO.puts fizz_word.(14)
+IO.puts fizz_word.(15)
+IO.puts fizz_word.(16)
